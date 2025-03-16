ab9de1147d2b38d3948ad9815f58b0f2fe71024b
Sumit Datta <111019954+DaInfernalCoder@users.noreply.github.com>
2025-03-16 08:02:46 -0500

    Initial Commit

---
+++ seafood/seafoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  seafoodApp.swift
+//  seafood
+//
+//  Created by Sumit Datta on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct seafoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
